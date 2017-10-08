538d7fac2981c1837f5ce71331d4e21e88c46db7
brendan <alber123@mail.chapman.edu>
2017-10-08 11:35:24 -0700

    updated JavaDocs

--- app/src/main/java/com/example/balbert/todo2day/TaskListAdapter.java
+++ app/src/main/java/com/example/balbert/todo2day/TaskListAdapter.java
@@ -14,6 +14,20 @@ import android.widget.CheckBox;
 import java.util.List;
 
 /**
+ *
+ * The TaskListAdapter class is for creating a custom way to display each Task.
+ *
+ * The heavy lifting of this class takes place in the getView method.
+ * getView retrieves a selected Task, inflates the View for that Task,
+ * gets a reference to its CheckBox and provides the appropriate
+ * description text and isDone state (checked or unchecked).
+ *
+ * A new feature of this app from the others worked on before is setting a tag.
+ * A tag is an invisible locker behind the select View where we can store
+ * properties or data for retrieval.
+ *
+ * getView then returns the inflated View.
+ *
  * Created by balbert on 10/3/2017.
  */
 
@@ -30,6 +44,22 @@ public class TaskListAdapter extends ArrayAdapter<Task> {
         mTaskList = objects;
     }
 
+    /**
+     *
+     *  getView() takes three parameters.
+     *  The first parameter, which is also the only one being used, is the position in the list
+     *  of the View which is being inflated.
+     *  The other two parameters, convertView and parent, are used to reuse the old view if
+     *  possible or necessary, and the parent is where this View will be attached.
+     *
+     *  The View is inflated and looks like a CheckBox with text from the user-provided
+     *  description alongside the CheckBox.
+     *
+     * @param position is the particular Task the user tapped on
+     * @param convertView is not used for the purposes of this app.
+     * @param parent is the parent on which this View will be attached.
+     * @return
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
